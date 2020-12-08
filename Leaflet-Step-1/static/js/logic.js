e32754d0c0b3f49ebd6f9301e0019403c18c5ec0
Kimberly Flores <kimberlyflores@Kimberlys-MBP.localdomain>
2020-12-07 21:30:02 -0600

    adding in circles

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,31 +1,29 @@
 // Store our API endpoint inside queryUrl
 var queryUrl =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-//"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-11-16&endtime=" +
-//   "2020-11-17&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
-
- // "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, create a geoJSON layer containing the features array and add a popup for each marker
   // then, send the layer to the createMap() function.
-  var earthquakes = L.geoJSON(data.features, {
-    onEachFeature : addPopup
-  });
+  var earthquakes = L.geoJSON(data.features)//, {
+  //   onEachFeature : addPopup
+  // });
 
-  createMap(earthquakes);
+  createMap(earthquakes, data.features);
 });
 
 
 // Define a function we want to run once for each feature in the features array
-function addPopup(feature, layer) {
-  // Give each feature a popup describing the place and time of the earthquake
-  return layer.bindPopup(`<h3> ${feature.properties.place} </h3> <hr> <p> ${Date(feature.properties.time)} </p>`);
-}
+// function addPopup(feature, layer) {
+//   // Give each feature a popup describing the place and time of the earthquake
+//   return layer.bindPopup(`<h3> ${feature.properties.place} </h3> <hr> <p>Date: ${Date(feature.properties.time)} (UTC)</p> 
+//   <p>Magnitude: ${feature.properties.mag} ml</p>
+//   <p>Depth: ${feature.geometry.coordinates[2]} km</p>
+//   <a href="${feature.properties.url}" target="_blank">More details...</a>`);
+// }
 
 // function to receive a layer of markers and plot them on a map.
-function createMap(earthquakes) {
+function createMap(earthquakes, data) {
 
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -47,24 +45,63 @@ function createMap(earthquakes) {
   };
 
   // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    "Earthquakes": earthquakes
-  };
+  // var overlayMaps = {
+  //   "Earthquakes": earthquakes
+  // };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 5,
-    layers: [streetmap, earthquakes]
+    layers: [streetmap]
   });
 
+    // Create the circles for each data point
+    data.forEach(function add_circles(earthdata){
+
+      var color = ""
+        if (earthdata.geometry.coordinates[2]< 10) {
+          color = "#009900";
+      }
+      else if (earthdata.geometry.coordinates[2] < 30) {
+          color = "#00FF00";
+      }
+      else if (earthdata.geometry.coordinates[2] < 50) {
+          color = "#e7d45c";
+      }
+      else if (earthdata.geometry.coordinates[2] < 70) {
+          color = "#e3a340";
+      }
+      else if (earthdata.geometry.coordinates[2] < 90) {
+          color = "#ec250f";
+      }
+      else {
+          color = "#d6d6d6";
+      }
+
+      // add circles to map
+      L.circle([earthdata.geometry.coordinates[1], earthdata.geometry.coordinates[0]], {
+        fillOpacity: .8,
+        color: "black",
+        fillColor: color,
+        // Adjust radius
+        radius: earthdata.properties.mag * 20000
+    }).bindPopup(`<h3>Name: ${earthdata.properties.title}</h3> <hr> 
+        <p>Date: ${Date(earthdata.properties.time)} (UTC)</p> 
+        <p>Magnitude: ${earthdata.properties.mag} ml</p>
+        <p>Depth: ${earthdata.geometry.coordinates[2]} km</p>
+        <a href="${earthdata.properties.url}" target="_blank">More details...</a>`)
+        .addTo(myMap);
+
+
+    });
   
 
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+  // L.control.layers(baseMaps, overlayMaps,{
+  //   collapsed: false
+  // }).addTo(myMap);
 }
